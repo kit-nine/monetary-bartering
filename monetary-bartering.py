4c6ae7853075c5cc89ae61a2dca4321dbe169c17
kit <kit_nine@outlook.com>
2023-05-25 11:12:58 -0400

    fin~
    added readme and removed the explanation from the beginning of the file

--- monetary-bartering.py
+++ monetary-bartering.py
@@ -1,16 +1,3 @@
-"""
-a ʇ is a unit, and each item is valued in units based on its
-ppm in the earth's crust, lower ppms becoming higher ʇs. the
-conversion between ppm and ʇ is the reciprocal of ppm / 1000000.
-
-to figure out something's value in units, first figure out
-what percentage of the object is made up of what elements.
-input the weight of the object, the elements that the object
-consists of, and the percentages of each element in the object.
-
-then divide the value by avogadro's number * 100000 and round it off to the hundredths place
-"""
-
 MILLION = 1000000
 
 def ppm_to_units(ppm):
